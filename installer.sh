2bf794c4fbbe585e28c59f586c06de7e44280e47
ZEYTIN <muratzeytin1997@hotmail.com>
2019-05-18 14:59:26 +0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,17 @@
+echo '************************'
+echo ' Flash Player Installer '
+echo '************************'
+
+echo .
+echo .
+echo 'Unziping File'
+tar -xf flashplayermanual.tar.xz
+echo 'Unziped File'
+echo 'Copying File'
+mv libflashplayer.so /usr/lib/mozilla/plugins/
+echo 'Deleting Template Files'
+rm -r libflashplayer.so
+rm -r license.pdf
+echo 'OK!'
+exit
+exit
