d3d7348c4d525a5a82a5bdc1b306462a2e155f9f
ZEYTIN <muratzeytin1997@hotmail.com>
2019-05-18 23:37:21 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,6 +8,7 @@ echo 'Unziping File'
 tar -xf flashplayermanual.tar.xz
 echo 'Unziped File'
 echo 'Copying File'
+sleep 5
 mv libflashplayer.so /usr/lib/mozilla/plugins/
 echo 'Deleting Template Files'
 rm -r libflashplayer.so
